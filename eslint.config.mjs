1d7c8d9868cd993ab91d9e8d2f9b937c0b9884db
shin4488 <efshinya48@gmail.com>
2025-09-17 23:02:31 +0900

    change: add extra rules for lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 
-export default [
+export default defineConfig(
   { ignores: ['node_modules', 'public', 'coverage', '.parcel-cache'] },
 
   // まず TSParser に tsconfig を認識させる
@@ -20,6 +21,21 @@ export default [
     },
   },
 
+  {
+    rules: {
+      'no-console': 'warn',
+      'no-extra-semi': 'warn',
+      'dot-notation': 'warn',
+      'prefer-const': 'error',
+      'no-unreachable-loop': 'error',
+      'no-var': 'error',
+      // if 1行も波括弧必須
+      curly: ['error', 'all'],
+      // (obj?.foo)?.() を要求
+      'no-unsafe-optional-chaining': 'error',
+    },
+  },
+
   // 型情報ありの推奨セット（TS/TSXにだけスコープする）
   ...tseslint.configs.recommendedTypeChecked.map((config) => ({
     ...config,
@@ -28,4 +44,4 @@ export default [
 
   // Prettier 競合OFF
   eslintConfigPrettier,
-];
+);
