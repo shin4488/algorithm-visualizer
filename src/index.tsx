18571ebc068caa7440d74ae79752f05305cb4778
shin4488 <efshinya48@gmail.com>
2025-09-17 23:34:56 +0900

    change: stop using dom manipulation directly

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,7 @@
-import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { init } from './visualizer';
-
-const Root: React.FunctionComponent = () => {
-  useEffect(() => {
-    init();
-  }, []);
-  return <App />;
-};
 
 const container = document.getElementById('root');
 if (container instanceof HTMLElement) {
-  const root = createRoot(container as Element);
-  root.render(<Root />);
+  createRoot(container).render(<App />);
 }
