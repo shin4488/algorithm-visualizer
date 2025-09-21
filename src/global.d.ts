c61e3aa9924c558596af55c44144279c14e3cb5c
shin4488 <efshinya48@gmail.com>
2025-09-21 10:22:17 +0900

    change: remove unsed global type definition

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,2 @@
 // tsxファイル内で.cssファイルをimportできるようにする
 declare module '*.css';
-
-declare global {
-  interface Window {
-    // 今回使うのは 'event' だけなので最小のシグネチャで定義
-    gtag?: (command: 'event', eventName: string, params?: Record<string, unknown>) => void;
-  }
-}
