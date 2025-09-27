d82dfad1952d61c79e23065b5b58e01a297e84e3
shin4488 <efshinya48@gmail.com>
2025-09-27 06:37:48 +0000

    add: import file alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vitest/config';
+import * as path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setupTests.ts',
